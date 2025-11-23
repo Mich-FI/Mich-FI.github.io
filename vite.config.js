8a66b95eec1d5bf7c606efe526d8298283445384
Mich-FI <mikaelgaram@gmail.com>
2025-11-23 22:41:03 +0200

    added other subfolders to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ export default defineConfig({
       input: {
         main: './index.html',
         cybersec_projects: './subsites/cybersec_projects/index.html',
+        minihtmlcssprojects: './subsites/minihtmlcssprojects/index.html',
+        portfoliodev: './subsites/portfoliodev/index.html',
+        tech_projects: './subsites/techprojects/index.html',
       },
     },
   },
